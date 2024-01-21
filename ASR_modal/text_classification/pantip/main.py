03bb84724d35f39d36db7e2035a12775e0812481
dearxcorex <deardevx@gmail.com>
2024-01-21 23:24:36 +0700

    still error

--- ASR_modal/text_classification/pantip/main.py
+++ ASR_modal/text_classification/pantip/main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException,StaleElementReferenceException
 import time
 
 import re 
@@ -58,7 +58,9 @@ class PANTIP_Automation:
             # fine name katoo
             all_ul = "//ul[@class='pt-list pt-list-item__full-title pt-list__type-a']"
             ul_elememnt =WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, all_ul)))
-            
+            # Store the ID of the original window
+            original_window = self.driver.current_window_handle
+  
             #get number comment
             for index,span_element in enumerate(ul_elememnt.find_elements(By.CLASS_NAME,"pt-li_stats-comment")):
                 desesired_text = re.sub(r"message", "", span_element.text)
@@ -66,14 +68,39 @@ class PANTIP_Automation:
                 if desesired_text >= 5:
                     #get post text and filter comment >5
                     get_text = ul_elememnt.find_elements(By.TAG_NAME, "h2")[index]
+
                     print(get_text.text)
                     time.sleep(3)
+                    
                     get_text.click()
+                    #print(len(self.driver.window_handles))
+                    self.driver.switch_to.window(self.driver.window_handles[1])
+
+                    #get text in comment
+                    get_id = "comments-jsrender"
+                    
+                    try:
+                        comments_container = WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.ID, get_id)))
+                        time.sleep(3)
+                        all_comment_elements = comments_container.find_elements(By.XPATH, "//*[@class='display-post-story-wrapper comment-wrapper']")
+                      
+                        comment_texts = [element.text for element in all_comment_elements if element.text]
+                        print(comment_texts)
+                        
+                    except TimeoutException:
+                        print("Comments container not found within timeout.")       
+                   
+                            
+
+                    
+                    
+
+             
 
                     # get text in comment 
                     all_comment = "display-post-story-wrapper comment-wrapper"
                     #https://www.selenium.dev/documentation/webdriver/interactions/windows/
-                   
+                              
 
          
                
