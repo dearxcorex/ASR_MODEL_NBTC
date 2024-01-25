d98744d7485ced3715dbecac215255683517f492
dearxcorex <deardevx@gmail.com>
2024-01-25 22:07:19 +0700

    deal with error

--- ASR_modal/text_classification/pantip/main.py
+++ ASR_modal/text_classification/pantip/main.py
@@ -1,38 +1,20 @@
-
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException, TimeoutException,StaleElementReferenceException
+from selenium.common import exceptions
 import time
 
 import re 
-# driver = webdriver.Chrome()
-# driver.get("https://pantip.com/tag/ยา")
 
-# try:
-#     # Wait for the title to contain "ยา" (expected title)
-#     WebDriverWait(driver, 10).until(EC.title_contains("ยา"))
-
-#     # Find the element using a more descriptive XPATH
-#     elem = WebDriverWait(driver, 10).until(
-#         EC.element_to_be_clickable((By.XPATH, "//a[text()='คลังกระทู้โปรด']"))
-#     )
-#     elem.click()
-
-#     # Ensure "No results found." is not present after clicking
-#     assert "No results found." not in driver.page_source
-
-# finally:
-#     print("done")
-    # driver.quit()  # Close the browser gracefully
 
 
 class PANTIP_Automation:
     def __init__(self) -> None:
         self.driver = self.initialize_driver()
+        self.driver.set_window_size(1500,1328)
 
 
     def initialize_driver(self) -> webdriver.Chrome:
@@ -53,8 +35,19 @@ class PANTIP_Automation:
             elem = WebDriverWait(self.driver, 10).until(
                 EC.element_to_be_clickable((By.XPATH, "//a[text()='คลังกระทู้โปรด']"))
             )
-           
+         
+            time.sleep(3)
+            # elem_list = WebDriverWait(self.driver, 10).until(
+            #     EC.element_to_be_clickable((By.XPATH, "//*[@id='gtm-topic-layout-headline']"))
+            # ).click()
+            
             elem.click()
+            
+            # elem_list.click()
+
+            # time.sleep(3)
+           
+          
             # fine name katoo
             all_ul = "//ul[@class='pt-list pt-list-item__full-title pt-list__type-a']"
             ul_elememnt =WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, all_ul)))
@@ -62,35 +55,43 @@ class PANTIP_Automation:
             original_window = self.driver.current_window_handle
   
             #get number comment
-            for index,span_element in enumerate(ul_elememnt.find_elements(By.CLASS_NAME,"pt-li_stats-comment")):
-                desesired_text = re.sub(r"message", "", span_element.text)
-                desesired_text = int(desesired_text)
-                if desesired_text >= 5:
-                    #get post text and filter comment >5
-                    get_text = ul_elememnt.find_elements(By.TAG_NAME, "h2")[index]
-
-                    print(get_text.text)
-                    time.sleep(3)
-                    
-                    get_text.click()
-                    #print(len(self.driver.window_handles))
-                    self.driver.switch_to.window(self.driver.window_handles[1])
-
-                    #get text in comment
-                    get_id = "comments-jsrender"
-                    
-                    try:
-                        comments_container = WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.ID, get_id)))
-                        time.sleep(3)
-                        all_comment_elements = comments_container.find_elements(By.XPATH, "//*[@class='display-post-story-wrapper comment-wrapper']")
-                      
-                        comment_texts = [element.text for element in all_comment_elements if element.text]
-                        print(comment_texts)
-                        break
+            span_elements = ul_elememnt.find_elements(By.CLASS_NAME,"pt-li_stats-comment")
+            for index,span_element in enumerate(span_elements):
+                try:
+                    desesired_text = re.sub(r"message", "", span_element.text)
+                    print(desesired_text)
+                    desesired_text = int(desesired_text)
+                    if desesired_text >= 5:
+                        #get post text and filter comment >5
                         
-                    except StaleElementReferenceException:
-                        print("Comments container not found within timeout.")       
+                    # WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.TAG_NAME, "h2")))
+                        get_text = ul_elememnt.find_elements(By.TAG_NAME, "h2")[index]
+                        print(get_text.text)
+                        time.sleep(3)
+                        
+                        get_text.click()
+                        print("just clicked")
+                        #print(len(self.driver.window_handles))
+                        self.driver.switch_to.window(self.driver.window_handles[1])
+
+                        #get text in comment
+                        get_id = "comments-jsrender"
+                        
+                        comments_container = WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.ID, get_id)))
+                        #time.sleep(3)
+                        all_comment_elements = comments_container.find_elements(By.XPATH, "//*[@class='display-post-story-wrapper comment-wrapper']")
+                        
+                        if all_comment_elements:
+                            comment_texts = [element.text for element in all_comment_elements if element.text]
+                            print(comment_texts)
+                        else:
+                            print("Comments container not found.")
+                
+                
+                except exceptions.StaleElementReferenceException as e:
+                        print(f"{e}Comments container not found within timeout.")       
                    
+                        print("dearxoasis")
                             
 
                     
@@ -98,9 +99,9 @@ class PANTIP_Automation:
 
              
 
-                    # get text in comment 
-                    all_comment = "display-post-story-wrapper comment-wrapper"
-                    #https://www.selenium.dev/documentation/webdriver/interactions/windows/
+                    # # get text in comment 
+                    # all_comment = "display-post-story-wrapper comment-wrapper"
+                    # #https://www.selenium.dev/documentation/webdriver/interactions/windows/
                               
 
          
